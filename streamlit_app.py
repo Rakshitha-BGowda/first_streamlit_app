a5db9d6a46eed5b20709268668904e33cda96a3a
Rakshitha-BGowda <141007423+Rakshitha-BGowda@users.noreply.github.com>
2023-07-31 12:44:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 import streamlit
 streamlit.title('My Parents New healthy Dinner')
-print("Breakfast Menu \n Omega 3 & Blueberry Oatmeal \n Kale, Spinach & Rocket Smoothie \n Hard-Boiled Free-Range Egg")
+print("Breakfast Menu ")
